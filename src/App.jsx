72dacff5e094f5318171d4d95976f0cc64bcdcdb
riskihia <riskirahmathia3@gmail.com>
2025-05-04 18:21:16 +0700

    feat: create App main screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import Main from "./Components/Main";
+import Header from "./Components/Header";
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
 }
