c7e711ee1e132c7b72b97b6627049646a20c37bc
riskihia <riskirahmathia3@gmail.com>
2025-05-04 20:30:10 +0700

    feat: create Option comp, show list of option
    - create index state
    - render list of option

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -1,5 +1,12 @@
-function Question() {
-  return <div>Question</div>;
+import Options from "./Options";
+
+function Question({ question }) {
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Options question={question} />
+    </div>
+  );
 }
 
 export default Question;
