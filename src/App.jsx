1d7eef1984caf64cf3d9a74361eb98cf7de4a03a
riskihia <riskirahmathia3@gmail.com>
2025-05-04 21:02:29 +0700

    feat: implement logic question, correct option & points
    - create new state answer, points to save correct option and user points
    - create new type of reducer newAnswer, to triger logic of question
    - style the option based on answer and question correct option

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,14 @@ const initialState = {
   //loading, error, ready,active,finished
   status: "loading",
   index: 0,
+  answer: null,
+  points: 0,
 };
 function reducer(state, action) {
+  const question = state.questions.at(state.index);
   switch (action.type) {
     case "dataReceived":
+      console.log(action.payload);
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return {
@@ -24,13 +28,22 @@ function reducer(state, action) {
       };
     case "start":
       return { ...state, status: "active" };
+    case "newAnswer":
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -53,7 +66,13 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
