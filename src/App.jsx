d5087c58a92df630b6dd5a11c7736461e3e929b5
riskihia <riskirahmathia3@gmail.com>
2025-05-04 18:52:10 +0700

    feat: fetch API questions & setup useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,36 @@
 import Main from "./components/Main";
 import Header from "./components/Header";
+import { useEffect } from "react";
+import { useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  //loading, error, ready,active,finished
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch(`http://localhost:8000/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch(() => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
